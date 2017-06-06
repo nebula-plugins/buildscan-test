be21686775ea4a3b88b72893743f105d2f5ce0e5
Rob Spieldenner <rspieldenner@netflix.com>
2017-06-06 15:57:20 -0700

    encrypted variable to see what buildscan does with it

---
+++ src/test/java/test/nebula/FooTest.java
@@ -0,0 +1,12 @@
+package test.nebula;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FooTest {
+    @Test
+    public void canary() {
+        assertEquals("1 == 1", 1,1);
+    }
+}
